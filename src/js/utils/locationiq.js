914895e9b98eea8fa0d4659d05e15ccfbd693278
beltranrengifo <beltran@rengifo.es>
2020-04-21 20:50:22 +0200

    deploy

--- src/js/utils/locationiq.js
+++
@@ -1,8 +0,0 @@
-const token = '31a0aa2ee4220c'
-const geolocationUrl = (lat, long) => (
-  `https://eu1.locationiq.com/v1/reverse.php?key=${token}&lat=${lat}&lon=${long}&format=json`
-)
-
-export {
-  geolocationUrl
-}

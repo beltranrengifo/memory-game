630cdb7b84455c884d5d066aa9ab32f463342e05
beltranrengifo <beltran@rengifo.es>
2020-04-25 11:42:22 +0200

    rm env from production in webpack

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,13 +2,6 @@ const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
-const dotenv = require('dotenv');
-
-const env = dotenv.config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 
 module.exports = merge(common, {
   mode: 'production',

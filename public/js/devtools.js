61a703563f3fe740d0ced5aa1276993ab66f3ffd
beltranrengifo <beltran@rengifo.es>
2020-04-25 19:06:37 +0200

    detect devtools with object property and request animation

--- public/js/devtools.js
+++ public/js/devtools.js
@@ -1,28 +1,20 @@
-!function () {
-  function detectDevTool (allow) {
-    if (isNaN(+allow)) allow = 100
-    var start = +new Date()
-    debugger
-    var end = +new Date()
-    if (isNaN(start) || isNaN(end) || end - start > allow) {
-      document.write('<div class="pirates" style="font-family: sans-serif; display: flex; align-items: center; justify-content: center; background: #3caea3; color: white; flex-direction: column; height: calc(100vh - 16px);"><h1>Dev tooling? uhm...</h1><p>Try again with devtools closed</p></div>')
-    }
+var checkStatus;
+var element = new Image();
+Object.defineProperty(element, 'id', {
+  get: function() {
+    checkStatus = true;
+    throw new Error("Dev tools checker");
   }
-  if (window.attachEvent) {
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-      detectDevTool()
-      window.attachEvent('onresize', detectDevTool)
-      window.attachEvent('onmousemove', detectDevTool)
-      window.attachEvent('onfocus', detectDevTool)
-      window.attachEvent('onblur', detectDevTool)
-    } else {
-      setTimeout(argument.callee, 0)
+});
+
+var body = document.querySelector('body')
+var bodyHtml = body.innerHTML
+
+requestAnimationFrame(function check () {
+    checkStatus = false;
+    console.dir(element);
+    if (checkStatus) {
+      body.innerHTML = '<div class="pirates" style="font-family: sans-serif; display: flex; align-items: center; justify-content: center; background: #3caea3; color: white; flex-direction: column; height: 100vh;"><h1>Dev tooling? uhm...</h1><p>Try again with devtools closed</p></div>'
     }
-  } else {
-    window.addEventListener('load', detectDevTool)
-    window.addEventListener('resize', detectDevTool)
-    window.addEventListener('mousemove', detectDevTool)
-    window.addEventListener('focus', detectDevTool)
-    window.addEventListener('blur', detectDevTool)
-  }
-}()
+    requestAnimationFrame(check);
+});
